a55f03ffd98c414cd79953298691c279f565c155
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-06 16:29:56 +0530

    Update blocked.py

--- EndAfk/AlphaDB/blocked.py
+++ EndAfk/AlphaDB/blocked.py
@@ -1,1 +1,29 @@
+from EndAfk import db
 
+blockdb = db.blocked
+
+async def block(a: int):
+    is_blocked = await blockdb.find_one({"a": a})
+    if is_blocked:
+        return
+    await blockdb.insert_one({"a": a})
+
+async def unblock(a: int):
+    is_blocked = await blockdb.find_one({"a": a})
+    if not is_blocked:
+        return
+    await blockdb.delete_one({"a": a})
+
+async def is_blocked(a: int): 
+    is_blocked = await blockdb.find_one({"a": a})
+    if is_blocked:
+        return True
+    else:
+        return False
+
+async def list_blocked():
+    _list = await blockdb.find({"a": {"$gt": 0}})
+    try:
+        return _list
+    except:
+        return []
