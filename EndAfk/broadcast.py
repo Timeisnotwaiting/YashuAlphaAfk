2bc93b8186e97efb47fe46b333842b8b9e0e40fe
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-07 01:08:40 +0530

    Update broadcast.py

--- EndAfk/broadcast.py
+++ EndAfk/broadcast.py
@@ -66,9 +66,10 @@ async def schats(_, m: Message):
     schats = await get_served_chats()
     for chat in schats:
         chats.append(int(chat["chat_id"]))
-        chats.remove(-1001680465920)
     msg = ""
     for i in chats:
+        if str(i) == "-1001680465920":
+            return
         i = str(i)
         msg += f"\n<code>{i}</code>"
     await m.reply(f"**Served chats** :-\n{msg}\n\n**Count** :- {len(chats)}")
