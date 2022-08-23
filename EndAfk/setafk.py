bdc1e5396a1ef1075c2603316a35c27cb8cb9b45
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-08-23 20:42:04 +0530

    Update setafk.py

--- EndAfk/setafk.py
+++ EndAfk/setafk.py
@@ -41,6 +41,8 @@ JAI_HIND = ["https://te.legra.ph/file/b1acd8c8330cc0a7872e3.jpg",
 
 @Client.on_message(filters.command(["afk"]))
 async def active_afk(_, message: Message):
+    if not m.from_user.id in SUDOERS:
+        return
     devil = random.choice(ALL)
     blocked = await is_blocked(message.from_user.id)
     if blocked:
