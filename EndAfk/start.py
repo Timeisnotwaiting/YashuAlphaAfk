54b3bcfed9f0cd9f0105a309b9a40f877ebee5e7
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-09-06 14:40:17 +0530

    Update start.py

--- EndAfk/start.py
+++ EndAfk/start.py
@@ -1,7 +1,7 @@
 import time
 import random
 from pyrogram import filters, Client
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from .afk import ALL
 from EndAfk import app, boot, botname
 from EndAfk.helpers import get_readable_time
@@ -12,6 +12,17 @@ alpha = random.choice(ALL)
 
 photo = "https://te.legra.ph/file/834b1444f48d090886fef.jpg"
 
+upl = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="➕ Add me to a Group",
+                        url=f"https://t.me/{botusername}?startgroup=true",
+                    ),
+                ]
+            ]
+        )
+
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, message: Message):
     blocked = await is_blocked(message.from_user.id)
@@ -19,7 +30,7 @@ async def start(_, message: Message):
         return await message.reply("you've been blocked try: ask @Timeisnotwaiting")
     first_name = message.from_user.first_name
     await message.reply_photo(alpha,
-       caption=f"Hey {first_name}! I'm Afk of @THE_END_NETWORK. \n\nTry: replying afk to some media else stickers to make it more reasonable !\n\n [‘ADD ME TO YOUR GROUP‘](https://t.me/endafkbot/?startgroup=true)")
+       caption=f"Hey {first_name}! I'm Afk of @THE_END_NETWORK. \n\nTry: replying afk to some media else stickers to make it more reasonable !\n\nFor help - @Timeisnotwaiting", reply_markup=upl)
 
 
 @Client.on_message(filters.command("ping") & filters.user(SUDOERS))
