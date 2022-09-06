596c6080070b484440270caf020765532505f0db
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-09-06 14:31:33 +0530

    Update start.py

--- EndAfk/start.py
+++ EndAfk/start.py
@@ -12,14 +12,14 @@ alpha = random.choice(ALL)
 
 photo = "https://te.legra.ph/file/834b1444f48d090886fef.jpg"
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("start") & filters.private)
 async def start(_, message: Message):
     blocked = await is_blocked(message.from_user.id)
     if blocked:
         return await message.reply("you've been blocked try: ask @Timeisnotwaiting")
     first_name = message.from_user.first_name
     await message.reply_photo(alpha,
-       caption=f"Hey {first_name}! I'm Afk of @THE_END_NETWORK. \n\nTry: replying afk to some media else stickers to make it more reasonable !\n\n @Timeisnotwaiting")
+       caption=f"Hey {first_name}! I'm Afk of @THE_END_NETWORK. \n\nTry: replying afk to some media else stickers to make it more reasonable !\n\n [ADD ME]("https://t.me/endafkbot/startgroup?=true")")
 
 
 @Client.on_message(filters.command("ping") & filters.user(SUDOERS))
