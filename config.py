911263b802ab7b96155f9ad8e3e7e226f0bad37b
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-19 12:01:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 SUDO_USER = list(
     map(int, getenv("SUDO_USER", "").split())
 ) 
+
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Timeisnotwaiting")
+
+if OWNER_USERNAME[0] == "@":
+    OWNER = OWNER_USERNAME[1:]
+else:
+    OWNER = OWNER_USERNAME
